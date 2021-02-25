538017b11dccd4a7278d8b786853f67221312f6a
Priyamvadha Padmakumar <pxp71710@ucmo.edu>
2021-02-25 09:08:08 -0600

    User-Index view created

---
+++ BookStore/wwwroot/js/user.js
@@ -0,0 +1,70 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    //$-shortcut for document.getElementById()- https://www.thoughtco.com/and-in-javascript-2037515
+    loadDataTable();
+});
+
+function loadDataTable() {
+    /*using table id from category Index view and 
+     * datatable.net software which we added to our project in _Layout.cshtml*/
+    dataTable = $('#tblData').DataTable({
+        //DataTable will only be recongnized if we add the corresponding javascript ref in _Layout.cshtml
+        /*Refer https://skillcrush.com/blog/what-is-ajax/ for understanding ajax*/
+        "ajax": {
+            "url": "/Admin/Category/GetAll" //Area/ControllerName/Action method or API
+        },
+        "columns": [
+            { "data": "name", "width": "60%" },
+            //naming inside datatable is lower camelcase. So, Name starts with 'n'
+            {
+                "data": "id",
+                //render 2 buttons/2 links(upsert and delete)
+                "render": function (data) {
+                    //return `(tie operator) used to return a div
+                    //first type the div portion in Index.cshtml to avoid spelling mistakes and check working
+                    //href="Admin/Category/Upsert/id(where we pass the data)
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Category/Upsert/${data}" data-toggle="tooltip" title="Edit" 
+                                   class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i> &nbsp;
+                                </a>
+                                <a onclick=Delete("/Admin/Category/Delete/${data}")
+                                   data-toggle="tooltip" title="Delete"
+                                   class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i> &nbsp;
+                                </a>
+                            </div>
+                       `;//semicolon must
+                }, "width": "40%"
+            }
+        ]
+    }); //semicolon - must (don't forget)
+}
+function Delete(url) {
+    //using sweetAlert for alert messages
+    swal({// see eg in https://sweetalert.js.org/guides/
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
