be486d45f1a7bed27d5591ab30e81fdfc556b02e
Priyamvadha Padmakumar <pxp71710@ucmo.edu>
2021-02-25 18:01:06 -0600

    ShoppingCartModel

---
+++ BookStoreModels/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreModels
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            Count = 1;
+        }
+        [Key]
+        public int Id { get; set; }
+        public string AppUserId { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public ApplicationUser ApplicationUser { get; set; }
+        public string BookId { get; set; }
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+        [Range(1,1000,ErrorMessage ="Please enter a value between 1 and 1000")]
+        public int Count  { get; set; }
+        [NotMapped]
+        public double Price { get; set; }
+
+    }
+}
